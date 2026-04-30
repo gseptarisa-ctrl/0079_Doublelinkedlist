89a99305a42b5f969cdbc234fdcd04cb1cee294d
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-04-30 21:14:46 +0700

    menambahkan node baru

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -14,9 +14,15 @@ public:
 class DoublyLinkedList
 {
     Node *START;
-    
+
 public:
     DoublyLinkedList()
     {
         START = NULL;
     }
+
+    void addnode()
+    {
+        int n;
+        cout << "Enter the roll number of the student: ";
+        cin >> n;
